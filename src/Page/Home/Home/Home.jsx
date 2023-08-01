b191b7002801b84f6096e3def144eeda6038da5a
rohan future devoloper <xrohaislam373@gmail.com>
2023-08-01 12:42:13 +0600

    review section

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -1,8 +1,8 @@
-import SectionTitles from "../../../Components/SectionTitles/SectionTitles";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Testimonials from "../Testimonials/Testimonials";
 
 
 const Home = () => {
@@ -13,8 +13,12 @@ const Home = () => {
       <Category></Category>
       <PopularMenu></PopularMenu>
      <Featured></Featured>
+     <Testimonials></Testimonials>
     </div>
   );
 };
 
 export default Home;
+
+
+
---
+++ src/Page/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,38 @@
+import SectionTitles from "../../../Components/SectionTitles/SectionTitles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section className="my-20">
+      <SectionTitles
+        subHeading={"What Our Client Say"}
+        heading={"testimonials"}
+      ></SectionTitles>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="mx-24 my-16 flex flex-col items-center">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <p className="py-8">{review.details}</p>
+              <h3 className="text-2xl text-orange-500">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
